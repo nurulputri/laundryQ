ca61cd362b5c996e9a786cdb5ce0441b95ce3aaf
Nurul Srianda Putri <nurulsriandaputri@gmail.com>
2022-05-07 09:41:04 +0700

    Initial Commit

---
+++ LaundryQ/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LaundryQ
+//
+//  Created by Nurul Srianda Putri on 07/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LaundryQUITests/LaundryQUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  LaundryQUITestsLaunchTests.swift
+//  LaundryQUITests
+//
+//  Created by Nurul Srianda Putri on 07/05/22.
+//
+
+import XCTest
+
+class LaundryQUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
